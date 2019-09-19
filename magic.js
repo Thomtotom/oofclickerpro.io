6e14d2f3cad96c2c54bcef1964f49816f882df1e
t-leith <leithtorrens@gmail.com>
2019-09-19 19:01:10 +1200

    bla

--- magic.js
+++ magic.js
@@ -14,7 +14,7 @@ $("#fart").click(function(){
 
 });
 
-
+// tom was here 
 $("#are").click(function(){
     // $("#are").append("<H1><font color="goldenrod"> you finised!! this one </font></H1>")
     $("#are").append("you finished")
@@ -25,4 +25,45 @@ $("#kik").click(function(){
     // $("#are").append("<H1><font color="goldenrod"> you finised!! this one </font></H1>")
     $("#kik").append("you finished")
     $("#kik").slideUp(5000)
+});
+
+$("#you").click(function(){
+    // $("#are").append("<H1><font color="goldenrod"> you finised!! this one </font></H1>")
+    $("#you").append("you finished")
+    $("#you").slideUp(5000)
+});
+
+$(document).ready(function(){
+    $("button").click(function(){
+      $("p").toggleClass("main");
+    });
+  });
+
+
+$("#but").click(function(){
+    $("#but").css("background-color", "DeepPink");
+    }, function(){
+    $("#but").css("background-color", "aqua");
+    }, function(){
+    $("#but").css("background-color", "blue");
+    }, function(){
+    $("#but").css("background-color", "purple");
+    }, function(){
+    $("#but").css("background-color", "black");
+  });
+
+
+
+$("#mop").click(function(){
+    var text = ""
+    for (i = 0; i < 10; i++) {
+        text += "bigger ";
+    }
+    $("#mop").append(text)
+
+});
+
+$("#mop").click(function(){
+    $("#mop").append("biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger biger ")
+
 });
